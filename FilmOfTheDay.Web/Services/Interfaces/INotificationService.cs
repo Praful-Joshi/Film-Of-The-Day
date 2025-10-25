29da669a44bc470bb5bad13b9b3fb99c0a6844e7
Praful-Joshi <prafuljoshi1010@gmail.com>
2025-10-25 13:54:07 -0400

    Updated notif controller and service.

--- FilmOfTheDay.Web/Services/Interfaces/INotificationService.cs
+++ FilmOfTheDay.Web/Services/Interfaces/INotificationService.cs
@@ -1,9 +1,10 @@
 using FilmOfTheDay.Core.Entities;
+using FilmOfTheDay.Web.Models.Notification;
 
 namespace FilmOfTheDay.Web.Services.Interfaces;
 public interface INotificationService
 {
     public Task CreateNotificationAsync(int userId, NotificationType type, string message, string? link = null);
-    public Task<IEnumerable<Notification>> GetUserNotificationsAsync(int userId);
+    public Task<NotificationViewModel> GetUserNotificationsAsync(int userId);
     public Task MarkAsReadAsync(int notificationId);
 }
